9e99f1c6d2a2833ce1fb2a0e4bc5ec7fedf186c8
tdp3 <donnelly.208@osu.edu>
2025-09-15 22:20:41 -0400

    the notes app

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// src/main.jsx (AFTER)
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+
+// --- Start of Amplify Configuration ---
+import { Amplify } from 'aws-amplify';
+import outputs from '../amplify_outputs.json';
+
+Amplify.configure(outputs);
+// --- End of Amplify Configuration ---
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
